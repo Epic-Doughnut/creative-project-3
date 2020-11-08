6a91aba1c26c7cb6947fa37a96241e0519015271
Joey <joeysherrill1387@gmail.com>
2020-11-07 23:56:16 -0600

    initilization and stuff

---
+++ src/mock-data.js
@@ -0,0 +1,27 @@
+let data = [{
+  id: 0,
+  name: 'black',
+  color: '#000000',
+},
+{
+  id: 1,
+  name: 'red',
+  color: '#ff0000'
+},
+{
+  id: 2,
+  name: 'blue',
+  color: '#0000ff'
+},
+{
+  id: 3,
+  name: 'green',
+  color: '#00ff00'
+},
+{
+  id: 4,
+  name: 'aqua',
+  color: '#00ffff'
+}
+
+]
